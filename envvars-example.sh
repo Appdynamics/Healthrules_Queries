930303216f843c2fe330820323bf0208caf55bcb
david.ryder@appdynamics.com <David.Ryder@AppDynamics.com>
2019-02-22 09:25:06 -0800

    Added example ennvironment variable

---
+++ envvars-example.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#
+# Authentication and Access
+export APPD_USER_NAME="david.ryder"
+export APPD_PWD="<user password>"
+export APPD_ACCOUNT="customer1"
+export APPD_GLOBAL_ACCOUNT="customer1_xy32d1ew..........."
+export APPD_CONTROLLER_HOST="<controler host>"
+export APPD_CONTROLLER_PORT="8090"
+export HTTP_PROTOCOL="http"
+export APPD_ACCESS_KEY=........-....-....-....-........"
+export APPD_API_CLIENT_SECRET="....-....-....-....-........."
+export APPD_API_CLIENT_NAME="david.ryder"
+#export APPD_TEMP_ACCESS_TOKEN=".................."
+export APPD_TEMP_ACCESS_TOKEN=""
+export APPD_APPLICATION_NAME="<AppDynamics Application Name>"
+export APPD_CONFIG_DIR="/Users/david.ryder/Documents/Projects/<AppName>"
